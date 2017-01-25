ca98bc818b91e9f9de4e1ea137651d588bb5a0aa
Matthisk Heimensen <m@tthisk.nl>
2017-01-25 17:45:50 +0100

    Added static root

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -81,6 +81,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 TEMPLATES = [
