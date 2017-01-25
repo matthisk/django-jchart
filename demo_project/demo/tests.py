e9964f172cf371b4266b707b38322f0ddf08284b
Matthisk Heimensen <m@tthisk.nl>
2017-01-25 16:08:11 +0100

    only run selenium locally

--- demo_project/demo/tests.py
+++ demo_project/demo/tests.py
@@ -1,3 +1,5 @@
+import os
+
 from django.test import TestCase, Client
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -12,8 +14,8 @@ class HomePageTestCase(TestCase):
     def test_home(self):
         response = self.client.get('/')
         self.assertContains(response, 'Django-Charting')
-        self.assertContains(response, '<canvas', count=8)
-        self.assertContains(response, 'new Chart(ctx, {', count=8)
+        self.assertContains(response, '<canvas', count=7)
+        self.assertContains(response, 'new Chart(ctx, {', count=7)
 
     def test_home_async(self):
         response = self.client.get('/async/')
@@ -21,36 +23,36 @@ class HomePageTestCase(TestCase):
         self.assertContains(response, '<canvas', count=6)
         self.assertContains(response, 'new Chart(ctx, configuration);', count=6)
 
+if not os.environ.get('CI', False):
+    class SeleniumTests(StaticLiveServerTestCase):
 
-class SeleniumTests(StaticLiveServerTestCase):
+        @classmethod
+        def setUpClass(cls):
+            super(SeleniumTests, cls).setUpClass()
+            cls.selenium = WebDriver()
+            cls.selenium.implicitly_wait(10)
 
-    @classmethod
-    def setUpClass(cls):
-        super(SeleniumTests, cls).setUpClass()
-        cls.selenium = WebDriver()
-        cls.selenium.implicitly_wait(10)
+        @classmethod
+        def tearDownClass(cls):
+            cls.selenium.quit()
+            super(SeleniumTests, cls).tearDownClass()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super(SeleniumTests, cls).tearDownClass()
+        def test_home(self):
+            self.selenium.get('%s%s' % (self.live_server_url, '/'))
+            self.selenium.find_element_by_class_name('chart')
+            logs = self.selenium.get_log('browser')
 
-    def test_home(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/'))
-        self.selenium.find_element_by_class_name('chart')
-        logs = self.selenium.get_log('browser')
+            for log in logs:
+                if 'favicon.ico' in log['message']:
+                    continue
+                self.assertEquals(log['message'], '')
 
-        for log in logs:
-            if 'favicon.ico' in log['message']:
-                continue
-            self.assertEquals(log['message'], '')
+        def test_home_async(self):
+            self.selenium.get('%s%s' % (self.live_server_url, '/async/'))
+            self.selenium.find_element_by_class_name('chart')
+            logs = self.selenium.get_log('browser')
 
-    def test_home_async(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/async/'))
-        self.selenium.find_element_by_class_name('chart')
-        logs = self.selenium.get_log('browser')
-
-        for log in logs:
-            if 'favicon.ico' in log['message']:
-                continue
-            self.assertEquals(log['message'], '')
+            for log in logs:
+                if 'favicon.ico' in log['message']:
+                    continue
+                self.assertEquals(log['message'], '')
