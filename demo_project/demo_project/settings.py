3db50ac2090e0f0cb60d8137eecd5fc64b18f9e1
Matthisk Heimensen <m@tthisk.nl>
2017-01-27 14:55:07 +0100

    Updated settings

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'django-charting.es6console.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-jchart.matthisk.nl']
 
 
 # Application definition
