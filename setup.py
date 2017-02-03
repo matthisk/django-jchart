2fba352b1195b6b75d831ed895dec380ca5425f8
Matthisk Heimensen <m@tthisk.nl>
2017-02-03 13:41:53 +0100

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras_require = {
 
 setup(
     name='django-jchart',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
